001150ed04352c66e28e6ab13d2ec1a88b6c27ba
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-05-07 14:24:16 +0200

    Update env-to-local-props.sh

--- scripts/env-to-local-props.sh
+++ scripts/env-to-local-props.sh
@@ -5,23 +5,16 @@ FILE="$GITHUB_WORKSPACE/local.properties"
 if [ -f "$FILE" ]; then
     echo "$FILE already exists, deleting"
     rm  "$FILE"
-    echo "Creating $FILE..."
-    {
-        echo "githubPackagesUsername=${{ secrets.GITHUB_USERNAME }}"
-        echo "githubPackagesPassword=${{ secrets.GITHUB_PASSWORD }}"
-#    echo "githubPackagesUsername=$GITHUB_USERNAME"
-#    echo "githubPackagesPassword=$GITHUB_PASSWORD"
-    } >>"$FILE"
-elif
-    echo "Creating $FILE..."
-    {
-        echo "githubPackagesUsername=${{ secrets.GITHUB_USERNAME }}"
-        echo "githubPackagesPassword=${{ secrets.GITHUB_PASSWORD }}"
-#    echo "githubPackagesUsername=$GITHUB_USERNAME"
-#    echo "githubPackagesPassword=$GITHUB_PASSWORD"
-    } >>"$FILE"
 fi
 
+echo "Creating $FILE..."
+{
+    echo "githubPackagesUsername=${{ secrets.GITHUB_USERNAME }}"
+    echo "githubPackagesPassword=${{ secrets.GITHUB_PASSWORD }}"
+#    echo "githubPackagesUsername=$GITHUB_USERNAME"
+#    echo "githubPackagesPassword=$GITHUB_PASSWORD"
+} >>"$FILE"
+    
 check_env_var() {
     if [ -z "${!1}" ]; then
         echo "Error: $1 environment variable is not set."
@@ -31,7 +24,5 @@ check_env_var() {
 
 check_env_var "GITHUB_USERNAME"
 check_env_var "GITHUB_PASSWORD"
-
-
 
 echo "$FILE created ✅"

d59b7b452afc4b2c6385aea865c22b67a691f3c7
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-05-07 14:22:22 +0200

    Update env-to-local-props.sh

--- scripts/env-to-local-props.sh
+++ scripts/env-to-local-props.sh
@@ -5,10 +5,6 @@ FILE="$GITHUB_WORKSPACE/local.properties"
 if [ -f "$FILE" ]; then
     echo "$FILE already exists, deleting"
     rm  "$FILE"
-    create_prop_file()
-fi
-
-create_prop_file() {
     echo "Creating $FILE..."
     {
         echo "githubPackagesUsername=${{ secrets.GITHUB_USERNAME }}"
@@ -16,7 +12,15 @@ create_prop_file() {
 #    echo "githubPackagesUsername=$GITHUB_USERNAME"
 #    echo "githubPackagesPassword=$GITHUB_PASSWORD"
     } >>"$FILE"
-}
+elif
+    echo "Creating $FILE..."
+    {
+        echo "githubPackagesUsername=${{ secrets.GITHUB_USERNAME }}"
+        echo "githubPackagesPassword=${{ secrets.GITHUB_PASSWORD }}"
+#    echo "githubPackagesUsername=$GITHUB_USERNAME"
+#    echo "githubPackagesPassword=$GITHUB_PASSWORD"
+    } >>"$FILE"
+fi
 
 check_env_var() {
     if [ -z "${!1}" ]; then

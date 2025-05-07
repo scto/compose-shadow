67739f6142eb81507e6a3d6e52c556955de06301
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-05-07 14:35:33 +0200

    Update env-to-local-props.sh

--- scripts/env-to-local-props.sh
+++ scripts/env-to-local-props.sh
@@ -1,18 +1,10 @@
-#!/bin/bash
+FILE="local.properties"
 
-FILE="$GITHUB_WORKSPACE/local.properties"
-          
 if [ -f "$FILE" ]; then
-    echo "$FILE already exists, deleting"
-    rm  "$FILE"
+    echo "$FILE already exists"
+    exit 0
 fi
 
-echo "Creating $FILE..."
-echo "githubPackagesUsername=${{ secrets.GITHUB_USERNAME }}" >> $GITHUB_WORKSPACE/local.properties
-echo "githubPackagesPassword=${{ secrets.GITHUB_PASSWORD }}" >> $GITHUB_WORKSPACE/local.properties
-#    echo "githubPackagesUsername=$GITHUB_USERNAME"
-#    echo "githubPackagesPassword=$GITHUB_PASSWORD"
-    
 check_env_var() {
     if [ -z "${!1}" ]; then
         echo "Error: $1 environment variable is not set."
@@ -22,5 +14,11 @@ check_env_var() {
 
 check_env_var "GITHUB_USERNAME"
 check_env_var "GITHUB_PASSWORD"
+
+echo "Creating $FILE..."
+{
+    echo "githubPackagesUsername=$GITHUB_USERNAME"
+    echo "githubPackagesPassword=$GITHUB_PASSWORD"
+} >>"$FILE"
 
 echo "$FILE created ✅"
